0d9b60acf7ccd2350736289889db10dd99403a60
alecsullivan <alecsullivan@gmail.com>
2016-10-18 11:37:52 -0700

    Fixing create service script.

--- scripts/createService.sh
+++ scripts/createService.sh
@@ -1,1 +1,1 @@
-[ -f /etc/init.d/id-service ] && echo "id-service already exists as an init.d service." || ln -s /opt/id-service/*.jar /etc/init.d/id-service
+ln -s /opt/id-service/*.jar /etc/init.d/id-service
