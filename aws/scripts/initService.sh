44d876b86ef069a3b205fe45f16b18f8442a95a4
alecsullivan <alecsullivan@gmail.com>
2016-10-28 10:10:46 -0700

    Creating .conf file

--- aws/scripts/initService.sh
+++ aws/scripts/initService.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 echo "Attempting to initialize id-service..."
+
+echo "Updating .conf file..."
+
+INSTANCE=`ec2-metadata | grep instance-id | cut -d ' '  -f2`
+REGION=`ec2-metadata | grep placement | cut -d ' '  -f2 | sed 's/.$//'`
+ENV=`ec2-describe-tags --region $REGION --filter "resource-type=instance" --filter "resource-id=$INSTANCE" --filter "key=Environment" | cut -d$'\t' -f5`
+echo "Located the Environment tag: $ENV"
+
+APP_NAME=`ls id-service*.jar | sed 's/.jar//g'`
+echo "JAVA_OPTS=\$JAVA_OPTS -Dspring.profiles.active=$ENV" > /var/id-service/$APP_NAME.conf
+echo "Created/updated JAVA_OPTS in $APP_NAME.conf."
+
 if [ ! -L /etc/init.d/id-service ]
 then
   echo "Creating id-service."
