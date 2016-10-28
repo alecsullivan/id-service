2e914f2587fafe26d7b07b267107c5605c0dce36
alecsullivan <alecsullivan@gmail.com>
2016-10-28 08:57:51 -0700

    Fixing start.sh to get AWS env

--- aws/scripts/start.sh
+++ aws/scripts/start.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+
+INSTANCE=`ec2-metadata | grep instance-id | cut -d ' '  -f2`
+REGION=`ec2-metadata | grep placement | cut -d ' '  -f2 | sed 's/.$//'`
+ENV=`ec2-describe-tags --region $REGION --filter "resource-type=instance" --filter "resource-id=$INSTANCE" --filter "key=Environment" | cut -d$'\t' -f5`
+
+export SPRING_PROFILES_ACTIVE=$ENV
+
 service id-service start
